6e533dc9d274eecedb3b7a9a441bd5962465c1b3
abobkov <abobkov@synergy.ru>
2020-06-08 17:57:29 +0300

    Второй коммит

--- index.js
+++ index.js
@@ -8,42 +8,57 @@ const delay = async (delayInms) => {
   });
 };
 
+// console.log("44 990 ₽".split(" "));
+
+const toNumber = (x) => {
+  const numberPattern = /\d+/g;
+  return Number.parseInt(x.match(numberPattern).join(""));
+};
+
+// console.log(toNumber("44 990 ₽"));
+
 const urls = [
   {
     name: "HUAWEI P30 PRO ЧЕРНЫЙ",
     url: "https://shop.huawei.ru/product/huawei-p30-pro",
     color:
       "div.color-circle[style='width: 25px;height: 25px;background: black; border-radius: 50%;']",
+    price: "44 990 ₽",
   },
   {
     name: "HUAWEI P30 PRO СЕВЕРНОЕ СИЯНИЕ",
     url: "https://shop.huawei.ru/product/huawei-p30-pro",
     color:
       "div.color-circle[style='width: 25px;height: 25px;background: #0d9ead; border-radius: 50%;']",
+    price: "44 990 ₽",
   },
   {
     name: "HUAWEI P30 PRO CВЕТЛО-ГОЛУБОЙ",
     url: "https://shop.huawei.ru/product/huawei-p30-pro",
     color:
       "div.color-circle[style='width: 25px;height: 25px;background: #bdf1ff; border-radius: 50%;']",
+    price: "44 990 ₽",
   },
   {
     name: "HUAWEI P30 PRO ДЫМЧАТЫЙ ЛАВАНДОВЫЙ",
     url: "https://shop.huawei.ru/product/huawei-p30-pro",
     color:
       "div.color-circle[style='width: 25px;height: 25px;background: #a67d94; border-radius: 50%;']",
+    price: "44 990 ₽",
   },
   {
     name: "HUAWEI P30 CВЕТЛО-ГОЛУБОЙ",
     url: "https://shop.huawei.ru/product/huawei-p30",
     color:
       "div.color-circle[style='width: 25px;height: 25px;background: #bdf1ff; border-radius: 50%;']",
+    price: "29 990 ₽",
   },
   {
     name: "HUAWEI P30 СЕВЕРНОЕ СИЯНИЕ",
     url: "https://shop.huawei.ru/product/huawei-p30",
     color:
       "div.color-circle[style='width: 25px;height: 25px;background: #0d9ead; border-radius: 50%;']",
+    price: "29 990 ₽",
   },
 ];
 
@@ -72,6 +87,7 @@ const priceCrawler = async () => {
       .then(async (browser) => {
         let page;
         // console.log("11113");
+        const aUrls = urls[i];
         const context = browser.defaultBrowserContext();
         const startTime = new Date().getTime();
         page = await browser.newPage();
@@ -79,22 +95,22 @@ const priceCrawler = async () => {
         await page.setDefaultNavigationTimeout(0);
         let flag = true;
         // if (i === 0 || urls[i - 1].url !== urls[i].url) {
-        await context.overridePermissions(urls[i].url, ["notifications"]);
-        await page.goto(urls[i].url, { waitUntil: "networkidle2" });
+        await context.overridePermissions(aUrls.url, ["notifications"]);
+        await page.goto(aUrls.url, { waitUntil: "networkidle2" });
         // }
         // console.log("Страница загружена");
-        if (urls[i].color) {
+        if (aUrls.color) {
           await page.evaluate((urls) => {
             document
               .querySelector(urls)
               .scrollIntoView({ block: "center", behavior: "smooth" });
             // document.scrollBy(0, -90);
-          }, urls[i].color);
-          await page.waitForSelector(urls[i].color, {
+          }, aUrls.color);
+          await page.waitForSelector(aUrls.color, {
             timeout: 5000,
           });
           await delay(1000);
-          await page.click(urls[i].color);
+          await page.click(aUrls.color);
           // console.log("Выбран цвет аппарата");
         }
         await delay(500);
@@ -104,10 +120,12 @@ const priceCrawler = async () => {
           })
           .then(() => {
             console.log(
+              "\x1b[36m",
               new Date().toLocaleString(),
               new Date().getTime() - startTime,
               urls[i].name,
-              "Нет в наличии"
+              `Нет в наличии`,
+              "\x1b[0m"
             );
             flag = false;
             browser.close();
@@ -126,7 +144,7 @@ const priceCrawler = async () => {
 
             await page.click("a.temp-cart-btn.btn_black_secondary");
 
-            // await delay(1000);
+            await delay(500);
             await page.waitForSelector("b#new-cart-price.label-detail", {
               timeout: 5000,
             });
@@ -141,12 +159,17 @@ const priceCrawler = async () => {
               "b#new-cart-price.label-detail",
               (elem) => elem.innerText
             );
+            let colorT = "\x1b[0m";
+            if (toNumber(nd) < toNumber(aUrls.price)) colorT = "\x1b[33m";
             console.log(
+              "\x1b[0m",
               new Date().toLocaleString(),
               new Date().getTime() - startTime,
               urls[i].name,
               "Есть в наличии, Стоимость:",
-              nd
+              colorT,
+              nd,
+              "\x1b[0m"
             );
             browser.close();
           });
@@ -155,7 +178,7 @@ const priceCrawler = async () => {
 
         // }
       });
-    await delay(1000);
+    await delay(2000);
   }
 };
 console.log("Запущено в ", new Date().toLocaleString());
@@ -165,4 +188,4 @@ setInterval(() => {
       "-------------------------------------------------------------------"
     )
   );
-}, 5 * 60 * 1000);
+}, 30 * 60 * 1000);
